6627d51cb885dc0cb348ab2ee9fe47961246b73a
Freddi2010 <feldhege.frederik@t-online.de>
2025-11-08 15:54:44 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -254,7 +254,7 @@ export default function EierPlattform() {
                   <div className={`w-5 h-5 rounded-full border-2 flex items-center justify-center ${lieferart === 'liefern' ? 'border-amber-600' : 'border-gray-400'}`}>
                     {lieferart === 'liefern' && <div className="w-3 h-3 rounded-full bg-amber-600"></div>}
                   </div>
-                  <span className="ml-3">Lieferung nach Hause (Nur Bermestr/Von Holte Str)</span>
+                  <span className="ml-3">Lieferung nach Hause</span>
                 </div>
               </div>
             </div>
